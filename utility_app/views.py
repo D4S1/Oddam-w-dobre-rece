5fe94c21038bfa8abb9fea0ef3dc785659ca6bdd
D4S1 <joanna.bastet@gmail.com>
2022-04-28 22:47:50 +0200

    dynamic counting and hrefs

--- utility_app/views.py
+++ utility_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Sum
+
+from . import models
 
 
 class LandingPageView(View):
 
     def get(self, request):
-        return render(request, 'utility_app/index.html')
+        bags = models.Donation.objects.aggregate(Sum('quantity'))['quantity__sum']
+        institutions = models.Institution.objects.all().count()
+        return render(request, 'utility_app/index.html', {
+            'bags': bags,
+            'institutions': institutions,
+        })
 
 
 class AddDonationView(View):
